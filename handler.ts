80902623c76513ee423d3d901f71637b3ee553c6
tmr08c <tmr08c@gmail.com>
2019-05-19 21:25:58 -0400

    Converts from HTTP endpoint to Schedule based.

--- handler.ts
+++ handler.ts
@@ -1,12 +1,9 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { ScheduledHandler } from "aws-lambda";
+import "source-map-support/register";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+export const generateWeeklySummary: ScheduledHandler = async (
+  _event,
+  _context
+) => {
+  console.log("schedule running...");
+};
